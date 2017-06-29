3091a0281ad4034fff262d04819f230ce5f108a3
yong.liu <ly_wuyi@163.com>
2017-06-29 17:37:43 +0800

    1. the header of page;

--- src/Header.js
+++ src/Header.js
@@ -10,11 +10,53 @@ class Header extends Component{
             <header className="site-header">
                 <div className="header-sides">
                     <div className="navbar">
-                        <div className="navbar-top"></div>
-                        <div className="navbar-middle">
-                            yong.liu
+                        <div className="navbar-top">
+                            <nav className="navbar-left">
+                                <ul className="navbar-menu">
+                                    <li>
+                                        <a href="#">About</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                            <nav className="navbar-right">
+                                <ul className="navbar-menu">
+                                    <li>
+                                        <a href="https://github.com/wedog">
+                                            <i className="fa fa-github fa-2x" aria-hidden="true"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(-1);">
+                                            <i className="fa fa-search fa-2x" aria-hidden="true"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
                         </div>
-                        <div className="navbar-bottom"></div>
+                        <div className="navbar-middle">
+                            Yong.Liu
+                        </div>
+                        <div className="navbar-bottom">
+                            <nav>
+                                <ul>
+                                    <li>
+                                        <a href="javascript:void(-1);">
+                                            home
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(-1);">
+                                            note
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(-1);">
+                                            any
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
                     </div>
                 </div>
             </header>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'font-awesome/css/font-awesome.css';
 import 'normalize.css';
 import './reset.css';
 import './index.css';
